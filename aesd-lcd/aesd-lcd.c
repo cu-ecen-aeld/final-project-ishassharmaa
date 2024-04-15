6d54937783501559513f8ed369fd3379ba01e0e3
Isha Sharma <123535278+ishassharmaa@users.noreply.github.com>
2024-04-14 19:46:55 -0600

    Update aesd-lcd.c

--- aesd-lcd/aesd-lcd.c
+++ aesd-lcd/aesd-lcd.c
@@ -14,23 +14,6 @@ Date: April 9, 2024
 
 #define LCD_RS  25               //Register select pin
 #define LCD_E   24               //Enable Pin
-<<<<<<< HEAD
-#define LCD_D4  23               //Data pin 4
-#define LCD_D5  22               //Data pin 5
-#define LCD_D6  21               //Data pin 6
-#define LCD_D7  14               //Data pin 7
- 
-int main()
-{
-    int lcd;               
-    wiringPiSetup();
-    wiringPiSetupGpio () ; 
-    
-    temp =20;       
-    lcd = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
-               
-    lcdPosition(lcd, 3, 1); 
-=======
 
 #define LCD_D4  23               //Data pin D4
 #define LCD_D5  22               //Data pin D5
@@ -47,22 +30,9 @@ int main()
     lcd = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
     
     lcdPosition(lcd, 2, 1); 
->>>>>>> 3f73da0a05c04c56dba37ae98a7971b749762875
     lcdPuts(lcd, "AESD PROJECT");   
     delay(1000);
     lcdClear(lcd);  
-<<<<<<< HEAD
-
-    lcdPuts(lcd, "TEMP SYSTEM"); 
-    sleep(2);
-    lcdClear(lcd);
-
-    lcdPrintf(lcd, "Temp: %d.0 C", temp);  
-}
-
-
-
-=======
 	delay(1000);
 	
 	lcdPosition(lcd, 2, 0); 
@@ -74,4 +44,3 @@ int main()
     lcdPrintf(lcd, "Temp: %.2f degC",temp );
     
 }
->>>>>>> 3f73da0a05c04c56dba37ae98a7971b749762875
