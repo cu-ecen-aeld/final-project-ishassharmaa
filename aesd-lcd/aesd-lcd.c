3f73da0a05c04c56dba37ae98a7971b749762875
ishassharmaa <issh2124@colorado.edu>
2024-04-14 18:39:22 -0600

    lcd working with 1 pot, able to print numbers

--- aesd-lcd/aesd-lcd.c
+++ aesd-lcd/aesd-lcd.c
@@ -14,32 +14,33 @@ Date: April 9, 2024
 
 #define LCD_RS  25               //Register select pin
 #define LCD_E   24               //Enable Pin
-#define LCD_D0  29               //Data pin D0
-#define LCD_D1  28               //Data pin D1
-#define LCD_D2  27               //Data pin D2
-#define LCD_D3  26               //Data pin D3
+
 #define LCD_D4  23               //Data pin D4
 #define LCD_D5  22               //Data pin D5
 #define LCD_D6  21               //Data pin D6
 #define LCD_D7  14               //Data pin D7
- 
+
+
+
 int main()
 {
-    int lcd;
-    temp =20;
-    wiringPiSetup();
-    lcd = lcdInit (2, 16, 8, LCD_RS, LCD_E, LCD_D0, LCD_D1, LCD_D2, LCD_D3, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
-
-    lcdPosition(lcd, 3, 1); 
+    int lcd;     
+    float temp =25.0;         
+    wiringPiSetup();        
+    lcd = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+    
+    lcdPosition(lcd, 2, 1); 
     lcdPuts(lcd, "AESD PROJECT");   
-    sleep(2);
+    delay(1000);
     lcdClear(lcd);  
-
-    lcdPuts(lcd, "TEMP SYSTEM"); 
-    sleep(2);
-    lcdClear(lcd);
-
-    lcdPrintf(lcd, "Temp: %d.0 C", temp); 
+	delay(1000);
+	
+	lcdPosition(lcd, 2, 0); 
+	lcdPuts(lcd, "TEMP SYSTEM"); 
+	delay(1000);
+    lcdClear(lcd); 
+    
+    lcdPosition(lcd, 0, 0); 
+    lcdPrintf(lcd, "Temp: %.2f degC",temp );
+    
 }
-
-
