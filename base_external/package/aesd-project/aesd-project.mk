95313b73dd59d443d4e1bd2a54ff01e1ff73f2b3
ishassharma <ishassharma@Ubuntu.myguest.virtualbox.org>
2024-04-21 07:51:49 +0530

    build with ref lcd code

--- base_external/package/aesd-project/aesd-project.mk
+++ base_external/package/aesd-project/aesd-project.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your aesd project git contents
-AESD_PROJECT_VERSION = 099c979647dc4b8373e380b68d21188b00e48e5e
+AESD_PROJECT_VERSION = 0fbd461bdc120029771bc4ff47af41137aed152d
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,12 +15,12 @@ AESD_PROJECT_SITE_METHOD = git
 AESD_PROJECT_GIT_SUBMODULES = YES
 
 define AESD_PROJECT_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/MLX_code all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/aesd-lcd all
 endef
 
 
 define AESD_PROJECT_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/MLX_code/* $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-lcd/aesd-lcd $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
