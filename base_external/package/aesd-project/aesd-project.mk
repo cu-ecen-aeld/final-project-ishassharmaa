b871e3eb33e0fec6de088b5aae213c0ea9959a0f
ishassharma <ishassharma@Ubuntu.myguest.virtualbox.org>
2024-04-29 10:49:59 +0530

    commit hash

--- base_external/package/aesd-project/aesd-project.mk
+++ base_external/package/aesd-project/aesd-project.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your aesd project git contents
-AESD_PROJECT_VERSION = 5e4ffff32f717daad08d2d89a8b817680156d964
+AESD_PROJECT_VERSION = 235d36d7614aa28f18060360f8c81c72302f6b22
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -21,7 +21,7 @@ endef
 
 define AESD_PROJECT_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-lcd/* $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/aesd-lcd/client-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdclient
+	
 endef
 
 $(eval $(generic-package))
